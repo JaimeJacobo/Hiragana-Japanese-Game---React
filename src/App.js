9ab4500a7651c6ed7a6477b8ff8e4cad73514e3a
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-08-14 01:57:57 +0200

    Setting up the basics for the Letters Game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Home from './components/Home/Home'
-import LettersGame from './components/LettersGame/LettersGame'
-import WordsGame from './components/WordsGame/WordsGame'
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import LettersGame from './components/LettersGame/LettersGame';
+import WordsGame from './components/WordsGame/WordsGame';
+import lettersJSON from './json/letters.json';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+	const [ letters, setLetters ] = useState(lettersJSON);
+
 	return (
 		<div className="App">
 			<Router>
 				<div>
-					<Route path="/" exact component={Home} />
-					<Route path="/letters" component={LettersGame} />
-					<Route path="/words" component={WordsGame} />
+					<Route path="/" exact component={() => <Home />} />
+					<Route path="/letters" component={() => <LettersGame letters={letters}/>} />
+					<Route path="/words" component={() => <WordsGame />} />
 				</div>
 			</Router>
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/LettersGame/LettersGame.js
+++ src/components/LettersGame/LettersGame.js
@@ -1,10 +1,86 @@
-import React from 'react';
-import './LettersGame.min.css'
+import React, { useState, useEffect } from 'react';
+import './LettersGame.min.css';
 
-const LettersGame = () => {
+const LettersGame = (props) => {
+	const letters = useState(props.letters)[0];
+	const [ correctAnswerObject, setCorrectAnswerObject ] = useState({ hiragana_letter: ':)' });
+	const [ previousAnswer, setPreviousAnswer ] = useState({ hiragana_letter: 'hola' });
+	const [ valueFromInput, setValueFromInput ] = useState('');
+	const [ feedbackAnswer, setFeedbackAnswer ] = useState('');
+
+	useEffect(() => {
+		getRandomLetter();
+	}, []);
+
+	const changePreviousAnswer = () => {
+		setPreviousAnswer(correctAnswerObject.hiragana_letter);
+	};
+
+	const getRandomIndex = () => {
+		return Math.floor(Math.random() * letters.length);
+	};
+
+	const checkForPreviousAnswer = (hiraganaLetter) => {
+		if (hiraganaLetter === previousAnswer) {
+			return false;
+		}
+		return true;
+	};
+
+	const getRandomLetter = () => {
+		changePreviousAnswer();
+		const randomIndex = getRandomIndex();
+		if (checkForPreviousAnswer(letters[randomIndex].hiragana_letter)) {
+			setCorrectAnswerObject(letters[randomIndex]);
+			renderRandomLetter();
+		} else {
+			getRandomLetter();
+		}
+	};
+
+	const renderRandomLetter = () => {
+		return correctAnswerObject.hiragana_letter;
+	};
+
+	const clearInputs = () => {
+		setValueFromInput('');
+		setFeedbackAnswer('');
+	};
+
+	const nextQuestion = () => {
+		setTimeout(() => {
+			getRandomLetter();
+			clearInputs();
+		}, 1000);
+	};
+
+	const getFeedbackMessage = (answer) => {
+		answer ? setFeedbackAnswer('Correcto!') : setFeedbackAnswer('Incorrecto :(');
+		nextQuestion();
+	};
+
+	const checkForAnswer = () => {
+		const inputAnswer = valueFromInput;
+		const correctAnswer = correctAnswerObject.latin_letter;
+		inputAnswer === correctAnswer ? getFeedbackMessage(true) : getFeedbackMessage();
+	};
+
+	const renderFeedback = ()=>{
+		return feedbackAnswer;
+	};
+
 	return (
 		<div className="LettersGame">
-			<h1>Letters Game</h1>
+			<div className="letter">{renderRandomLetter()}</div>
+			<input
+				type="text"
+				value={valueFromInput}
+				onChange={(event) => setValueFromInput(event.target.value)}
+			/>
+			<button className="inputButton" onClick={()=>checkForAnswer()}>Check answer</button>
+			<div className="feedback_container">
+				<p>{renderFeedback()}</p>
+			</div>
 		</div>
 	);
 };
