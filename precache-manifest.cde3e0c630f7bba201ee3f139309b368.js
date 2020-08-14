7a90e89298ec83ec64713d4a0e782cbe11e75a2d
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-08-14 08:33:55 +0200

    Updates

---
+++ precache-manifest.cde3e0c630f7bba201ee3f139309b368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f0ecfaaf29da7068c6dfd3d99e60e9",
+    "url": "/Hiragana-Japanese-Game---React/index.html"
+  },
+  {
+    "revision": "a4434e76dfb6bd7c12f5",
+    "url": "/Hiragana-Japanese-Game---React/static/css/main.3f369603.chunk.css"
+  },
+  {
+    "revision": "fbe5f09f21e9de2c9111",
+    "url": "/Hiragana-Japanese-Game---React/static/js/2.f6ee097d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hiragana-Japanese-Game---React/static/js/2.f6ee097d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4434e76dfb6bd7c12f5",
+    "url": "/Hiragana-Japanese-Game---React/static/js/main.bfda323d.chunk.js"
+  },
+  {
+    "revision": "8fe2ba948f62813a412c",
+    "url": "/Hiragana-Japanese-Game---React/static/js/runtime-main.23336087.js"
+  }
+]);
