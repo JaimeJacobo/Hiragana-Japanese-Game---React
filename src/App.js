dc26fd7f4bb0000186dfb1146efeefeb6456670d
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-08-14 16:33:26 +0200

    Trying to correct the vh problem with mobiles

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import WordsGame from './components/WordsGame/WordsGame';
 import lettersDayOne from './json/lettersDayOne.json';
 import lettersDayTwo from './json/lettersDayTwo.json';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Div100vh from 'react-div-100vh';
 
 const App = () => {
 	return (
-		<div className="App">
-			<Router>
-				<div>
-					<Route path="/" exact component={() => <Home />} />
-					<Route
-						exact
-						path="/letters"
-						component={() => (
-							<LettersGame letters={{ lettersDayOne: lettersDayOne, lettersDayTwo: lettersDayTwo }} />
-						)}
-					/>
-					<Route exact path="/words" component={() => <WordsGame />} />
-				</div>
-			</Router>
-		</div>
+		<Div100vh>
+			<div className="App">
+				<Router>
+					<div>
+						<Route path="/" exact component={() => <Home />} />
+						<Route
+							exact
+							path="/letters"
+							component={() => (
+								<LettersGame letters={{ lettersDayOne: lettersDayOne, lettersDayTwo: lettersDayTwo }} />
+							)}
+						/>
+						<Route exact path="/words" component={() => <WordsGame />} />
+					</div>
+				</Router>
+			</div>
+		</Div100vh>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
