59ebdb607a5ff34526b09c05bcbc20fd25b314e0
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-08-14 15:58:54 +0200

    Cleaning the imports

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.min.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
